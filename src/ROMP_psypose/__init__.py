9addb2652d395c7aa60bbe5f1fb5313419721880
Landry Bulls <bulls409@gmail.com>
2021-07-19 14:01:45 -0400

    fixing check_data_files import

--- src/ROMP_psypose/__init__.py
+++ src/ROMP_psypose/__init__.py
@@ -2,6 +2,6 @@ __author__ = """Landry Bulls """
 __email__ = 'Landry.S.Bulls@Dartmouth.edu'
 __all__ = ['configs', 'core', 'lib']
 
-from core.check_files import check_data_files
+from ROMP_psypose.core.check_files import check_data_files
 
 check_data_files()
