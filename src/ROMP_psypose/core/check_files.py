6da19e3867b4f98c8ed4e80bfe142f4d0b6117d5
Landry Bulls <bulls409@gmail.com>
2021-07-20 13:55:40 -0400

    fixing check_files

--- src/ROMP_psypose/core/check_files.py
+++ src/ROMP_psypose/core/check_files.py
@@ -8,55 +8,62 @@ from pathlib import Path
 import gdown
 
 gdrive_id = '1lCWXfvf2DwU9ck9SfiwJrknp7PSkkoPI'
-ROMP_DATA_DIR = Path(osp.join(osp.dirname(__file__), "ROMP"))
-trained_model_path = osp.join(ROMP_DATA_DIR, 'trained_models')
+#ROMP_DATA_DIR = Path(osp.join(osp.dirname(__file__), "ROMP"))
+ROMP_DATA_DIR = Path('~/.ROMP').expanduser()
+#trained_model_path = osp.join(ROMP_DATA_DIR, 'trained_models')
 
-def check_data_files(prompt_confirmation=False):
-    ROMP_DL = False
-    if osp.isdir(trained_model_path):
-        ROMP_DL = True
-    if not ROMP_DL:
-        if prompt_confirmation:
-            msg = (
-                f"ROMP needs to download {len(missing_files)} "
-                f"file{'s' if len(missing_files) > 1 else ''} in order "
-                f"to run:\n\t{', '.join(missing_files.keys())}"
-                "\n\tDo you want to download them now?\n[Y/n] \n"
-            )
-            while True:
-                response = input(msg).lower().strip()
-                if response in ('y', ''):
-                    confirmed = True
-                    break
-                elif response == 'n':
-                    confirmed = False
-                    break
-        else:
-            confirmed = True
-        if confirmed:
-            errors = {}
-            fname = 'ROMP_data.zip'
-            dest_path = ROMP_DATA_DIR.joinpath(fname)
-            print(f"downloading {fname} ...")
-            try:
-                download_from_gdrive(gdrive_id, dest_path)
-            except (MissingSchema, OSError) as e:
-                errors[fname[0]] = e
-            if any(errors):
-                print(
-                    f"Failed to download {len(errors)} files. See stack "
-                    f"trace{'s' if len(errors) > 1 else ''} below for "
-                    "more info:\n"
-                )
-            for fname, e in errors.items():
-                print(f"{fname.upper()}:")
-                traceback.print_exception(type(e), e, e.__traceback__)
-                print('=' * 40, end='\n\n')
-        else:
-            warnings.warn(
-                "missing required files. Some Psypose "
-                "functionality may be unavailable"
-            )
+# def check_data_files():
+#     if os.path.exists(ROMP_DATA_DIR):
+#         None
+#     else:
+#         get_ROMP_files()
+#
+# def check_data_files(prompt_confirmation=False):
+#     ROMP_DL = False
+#     if osp.isdir(trained_model_path):
+#         ROMP_DL = True
+#     if not ROMP_DL:
+#         if prompt_confirmation:
+#             msg = (
+#                 f"ROMP needs to download {len(missing_files)} "
+#                 f"file{'s' if len(missing_files) > 1 else ''} in order "
+#                 f"to run:\n\t{', '.join(missing_files.keys())}"
+#                 "\n\tDo you want to download them now?\n[Y/n] \n"
+#             )
+#             while True:
+#                 response = input(msg).lower().strip()
+#                 if response in ('y', ''):
+#                     confirmed = True
+#                     break
+#                 elif response == 'n':
+#                     confirmed = False
+#                     break
+#         else:
+#             confirmed = True
+#         if confirmed:
+#             errors = {}
+#             fname = 'ROMP_data.zip'
+#             dest_path = ROMP_DATA_DIR.joinpath(fname)
+#             print(f"downloading {fname} ...")
+#             try:
+#                 get_ROMP_files(gdrive_id, dest_path)
+#             except (MissingSchema, OSError) as e:
+#                 errors[fname[0]] = e
+#             if any(errors):
+#                 print(
+#                     f"Failed to download {len(errors)} files. See stack "
+#                     f"trace{'s' if len(errors) > 1 else ''} below for "
+#                     "more info:\n"
+#                 )
+#             for fname, e in errors.items():
+#                 print(f"{fname.upper()}:")
+#                 traceback.print_exception(type(e), e, e.__traceback__)
+#                 print('=' * 40, end='\n\n')
+#         else:
+#             warnings.warn(
+#                 "missing required files. Some Psypose "
+#                 "functionality may be unavailable"
+#             )
 
 
 def move_romp_files(extracted_dir):
@@ -74,12 +81,23 @@ def move_romp_files(extracted_dir):
     shutil.rmtree(extracted_dir)
 
 
-def get_ROMP_files(gdrive_id, dest_path):
+# def get_ROMP_files(gdrive_id, dest_path):
+#     url = f"https://drive.google.com/uc?id={gdrive_id}"
+#     gdown.download(url, str(dest_path), quiet=False)
+#     print(f"extracting {dest_path} ...")
+#     z = zipfile.ZipFile(str(dest_path))
+#     z.extractall(ROMP_DATA_DIR)
+#     move_romp_files(os.path.join(ROMP_DATA_DIR, 'ROMP_data'))
+#     print(f"removing {dest_path} ...")
+#     dest_path.unlink()
+
+def get_ROMP_files():
     url = f"https://drive.google.com/uc?id={gdrive_id}"
+    dest_path = ROMP_DATA_DIR.join('ROMP_data.zip')
     gdown.download(url, str(dest_path), quiet=False)
-    print(f"extracting {dest_path} ...")
+    print("extracting {dest_path} ...")
     z = zipfile.ZipFile(str(dest_path))
     z.extractall(ROMP_DATA_DIR)
-    move_romp_files(os.path.join(ROMP_DATA_DIR, 'ROMP_data'))
+    #move_romp_files(os.path.join(ROMP_DATA_DIR, 'ROMP_data'))
     print(f"removing {dest_path} ...")
     dest_path.unlink()
