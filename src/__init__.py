a4eeed50decd9d21eae1417acddf0a960eaa5a3e
Landry S. Bulls <bulls409@gmail.com>
2021-10-26 18:18:10 -0400

    deleting spurious __init__

--- src/__init__.py
+++
@@ -1,7 +0,0 @@
-__author__ = """Landry Bulls """
-__email__ = 'Landry.S.Bulls@Dartmouth.edu'
-__all__ = ['active_configs', 'configs', 'demo', 'docs', 'romp', 'scripts']
-
-from romp.lib.utils.check_files import get_ROMP_files
-
-get_ROMP_files()
